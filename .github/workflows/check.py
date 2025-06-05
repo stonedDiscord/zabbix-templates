4edf3b9fc47db542abf7a2d03951659d479a5d5a
stonedDiscord <Tukz@gmx.de>
2025-06-05 12:56:29 +0000

    fix check

--- .github/workflows/check.py
+++ .github/workflows/check.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import sys
 import time
-from zabbix.api import ZabbixAPI
+from pyzabbix import ZabbixAPI
 
 for x in range(20):
     try:
-        zapi = ZabbixAPI(url="http://localhost:8050/api_jsonrpc.php",
-                         user="Admin",
-                         password="zabbix")
+        zapi = ZabbixAPI(url="http://localhost:8050/")
+        zapi.login("Admin","zabbix")
     except:
         print('Zabbix 5.0 API is not ready...')
         time.sleep(5)
@@ -19,11 +18,36 @@ else:
   
 for x in range(20):
     try:
-        zapi = ZabbixAPI(url="http://localhost:8054/api_jsonrpc.php",
-                         user="Admin",
-                         password="zabbix")
+        zapi = ZabbixAPI(url="http://localhost:8054/")
+        zapi.login("Admin","zabbix")
     except:
         print('Zabbix 5.4 API is not ready...')
+        time.sleep(5)
+    else:
+        zapi.do_request('user.logout')
+        break;
+else:
+  sys.exit('Failed to wait for Zabbix API to be ready')
+
+for x in range(20):
+    try:
+        zapi = ZabbixAPI(url="http://localhost:8060/")
+        zapi.login("Admin","zabbix")
+    except:
+        print('Zabbix 6.0 API is not ready...')
+        time.sleep(5)
+    else:
+        zapi.do_request('user.logout')
+        break;
+else:
+  sys.exit('Failed to wait for Zabbix API to be ready')
+
+for x in range(20):
+    try:
+        zapi = ZabbixAPI(url="http://localhost:8064/")
+        zapi.login("Admin","zabbix")
+    except:
+        print('Zabbix 6.4 API is not ready...')
         time.sleep(5)
     else:
         zapi.do_request('user.logout')
